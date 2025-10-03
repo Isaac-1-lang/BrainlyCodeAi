99d025c133e124813f948b3cab40972fd443e00c
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-10-03 16:07:22 +0200

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -25,7 +25,7 @@ export class AuthController {
 
     res.cookie("refresh_token", refresh_token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
