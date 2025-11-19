64fae458f430a31055c5b493c280d0e85fb995fc
Joshua <izerejoshua94@gmail.com>
2025-11-19 20:53:55 +0200

    Update challenges.service.ts

--- src/challenges/challenges.service.ts
+++ src/challenges/challenges.service.ts
@@ -24,7 +24,7 @@ export class ChallengesService {
         cloudinary.uploader.upload(base64File ,
           {
             folder: "challenge-documents",
-            resource_type: "raw",
+            resource_type: "auto",  
             use_filename: true,
             public_id: cleanName,
             filename_override: cleanName,
